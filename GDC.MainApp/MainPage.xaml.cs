073cbb8e32f23046dab324f6f5e284af26b62191
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2024-01-25 10:25:18 +0200

    Добавьте файлы проекта.

---
+++ GDC.MainApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GDC.MainApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GDC.MainApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GDC.MainApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
