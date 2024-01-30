7a7a194426925af98ff8f6be96b8a5fa991cd8c7
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2024-01-30 16:49:04 +0200

    File check logic

---
+++ GDC.Plugins/Files/FileUpload.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDC.Plugins.Files
+{
+    public class FileUpload
+    {
+        string currentUploadFilePath = Path.Combine(FileSystem.Current.CacheDirectory, "uploads");
+        public void InitUploadDir() 
+        {
+            if (!Directory.Exists(currentUploadFilePath)) Directory.CreateDirectory(currentUploadFilePath);
+        }
+
+
+    }
+}
