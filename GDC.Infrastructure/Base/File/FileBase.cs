5ba44551eab82c1718af392518810deecbd2471f
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2024-02-02 01:41:46 +0200

    base

---
+++ GDC.Infrastructure/Base/File/FileBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GDC.Business;
+
+namespace GDC.Infrastructure.Base.File
+{
+    public class FileBase
+    {
+        string path = string.Empty;
+        public FileBase()
+        {
+            path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Uploads");
+        }
+
+        public async Task UploadFiles()
+        {
+            
+        }
+
+        public async Task DeleteFile()
+        {
+
+        }
+
+        public async Task ClearDirectory()
+        {
+
+        }
+    }
+}
