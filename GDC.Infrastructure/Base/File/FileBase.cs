9d0613ee7cbb4956ddf572e48b9625235d7fb208
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2024-02-02 01:54:14 +0200

    IBrowserFile Не работает в библиотеке MAUI

--- GDC.Infrastructure/Base/File/FileBase.cs
+++ GDC.Infrastructure/Base/File/FileBase.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GDC.Business;
+﻿using Microsoft.AspNetCore.Components.Forms;
 
 namespace GDC.Infrastructure.Base.File
 {
